4f61d22a064e122fb537ac6f5e3732756eba9f3f
triminhdev <triminhnguyen.dev@gmail.com>
2023-12-31 14:15:45 +0700

    eee

--- components/EpisodeButton.tsx
+++ components/EpisodeButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function EpisodeButton({ number }: { number: number }) {
   return (
     <Link href="/">
-      <div className="w-8 h-8 rounded-lg bg-[#191919] flex items-center justify-center">
+      <div className="w-8 h-8 font-bold rounded-lg bg-[#191919] flex items-center justify-center">
         {number}
       </div>
     </Link>
