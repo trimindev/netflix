ef90c675b5ececbd1639f7eebc520ead345a440a
triminhdev <triminhnguyen.dev@gmail.com>
2024-01-01 16:35:24 +0700

    e

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [""],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
